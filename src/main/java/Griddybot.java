cec50297eefba2f0b51fb53635b15298df169fa0
Sean Tan <e1408767@u.nus.edu>
2025-08-20 23:09:48 +0800

    Replace Duke with Griddybot

---
+++ src/main/java/Griddybot.java
@@ -0,0 +1,6 @@
+public class Griddybot {
+    public static void main(String[] args) {
+        String line = "_________________________________\n";
+        System.out.println(line + "Hello! I'm Griddybot.\n" + "What can I do for you?\n" + line + "Bye. Hope to see you again soon!\n" + line);
+    }
+}
