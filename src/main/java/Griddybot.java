0af893a173134bfd6e79e64638e86dcfe2ec2cd8
Sean Tan <e1408767@u.nus.edu>
2025-08-28 20:14:59 +0800

    Implement Add and List

--- src/main/java/Griddybot.java
+++ src/main/java/Griddybot.java
@@ -1,11 +1,14 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Griddybot {
     public static void main(String[] args) {
-        String line = "_________________________________\n";
-        System.out.println(line + "Hello! I'm Griddybot.\n" + "What can I do for you?\n" + line);
+        String line = "_________________________________" + System.lineSeparator();
+        System.out.println(line + "Hello! I'm Griddybot." + System.lineSeparator() + "What can I do for you?" + System.lineSeparator() + line);
 
         boolean isExit = false;
+        ArrayList<String> listItems = new ArrayList<>();
+        int counter = 0;
 
         while (!isExit) {
             String inputLine;
@@ -14,11 +17,20 @@ public class Griddybot {
 
             switch (inputLine) {
             case "bye":
-                System.out.println(line + "Bye. Come back soon!\n" + line);
+                System.out.println(line + "Bye. Come back soon!" + System.lineSeparator() + line);
                 isExit = true;
                 break;
+            case "list":
+                System.out.print(line);
+                for (String item : listItems) {
+                    counter++;
+                    System.out.print(counter + ". " + item + System.lineSeparator());
+                }
+                System.out.print(line);
+                break;
             default:
-                System.out.println(line + inputLine + "\n" + line);
+                listItems.add(inputLine);
+                System.out.println(line + "added: " + inputLine + System.lineSeparator() + line);
             }
         }
     }
