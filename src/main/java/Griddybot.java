ba50ae6a95e1b93d753ddd4ba5993dc3e5baed1c
Sean Tan <e1408767@u.nus.edu>
2025-08-28 19:44:38 +0800

    Implement Echo and Bye

--- src/main/java/Griddybot.java
+++ src/main/java/Griddybot.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Griddybot {
     public static void main(String[] args) {
         String line = "_________________________________\n";
-        System.out.println(line + "Hello! I'm Griddybot.\n" + "What can I do for you?\n" + line + "Bye. Hope to see you again soon!\n" + line);
+        System.out.println(line + "Hello! I'm Griddybot.\n" + "What can I do for you?\n" + line);
+
+        boolean isExit = false;
+
+        while (!isExit) {
+            String inputLine;
+            Scanner input = new Scanner(System.in);
+            inputLine = input.nextLine();
+
+            switch (inputLine) {
+            case "bye":
+                System.out.println(line + "Bye. Come back soon!\n" + line);
+                isExit = true;
+                break;
+            default:
+                System.out.println(line + inputLine + "\n" + line);
+            }
+        }
     }
 }
